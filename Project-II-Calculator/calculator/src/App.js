0002109d5508aac3789f3a8f8c0540737c5d6105
Max Webb <webbwrites@gmail.com>
2019-05-18 14:55:31 -0600

    Started some components and very basic styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton.js'
+import ActionButton from './components/ButtonComponents/ActionButton.js'
+import Display from './components/DisplayComponents/CalculatorDisplay.js'
 
-const App = () => {
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+
+function App() {
   return (
-    <div>
-
+    <div className="app">
+      <NumberButton value="7"/>
+      <ActionButton />
+      <Display />
     </div>
   );
 };
@@ -23,3 +30,10 @@ export default App;
 //     inside of this file in order to make them work.
 //   </strong>
 // </p>
+
+
+
+// {numbers.map(number => {
+//   <NumberButton value={number} key={number}/>
+// })
+// }
